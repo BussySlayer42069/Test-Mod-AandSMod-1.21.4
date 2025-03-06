f0ce87aaf4b529ca89df512b867f85e3b82abe73
BussySlayer42069 <ethaninbox.anderson@gmail.com>
2025-03-06 09:35:56 -0600

    Initial commit

---
+++ src/main/java/com/bussyslayer42069/archaeologyandsmellsmod/ArchaeologyAndSmellsMod.java
@@ -0,0 +1,15 @@
+package com.bussyslayer42069.archaeologyandsmellsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArchaeologyAndSmellsMod implements ModInitializer {
+	public static final String MOD_ID = "archaeology-and-smells-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/bussyslayer42069/archaeologyandsmellsmod/ArchaeologyandSmellsModClient.java
@@ -0,0 +1,10 @@
+package com.bussyslayer42069.archaeologyandsmellsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArchaeologyandSmellsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
